25312f1dc102feff896fae521d65e2ab2e71cc55
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-17 14:28:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@
 
 class CashRegister
   attr_accessor :discount, :total
-  
+
 
   def initialize(discount = 0)
     @total = 0
@@ -28,17 +28,17 @@ class CashRegister
     end
     @total -= @total * @discount / 100
     return "After the discount, the total comes to $#{@total}."
-  end 
+  end
 
   def items
     item_names = []
     @cart.each do | item_info |
       #test expects product name * quantity...
-      for qty in 1..item_info[:quantity] 
+      for qty in 1..item_info[:quantity]
         item_names << item_info[:name]
-      end 
-    end 
+      end
+    end
     item_names
-  end 
+  end
 
 end
